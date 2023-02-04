19500a54175a6c4d8ec488f16f110d898d8a4573
wuchangjun233 <2101546835@qq.com>
2023-02-04 19:56:18 +0800

    ✨ Ntchat支持（强绑定GenshinUID

--- nonebot_plugin_gspanel/__init__.py
+++ nonebot_plugin_gspanel/__init__.py
@@ -1,15 +1,19 @@
+import re
+from typing import Tuple
+
 from nonebot import get_driver
 from nonebot.log import logger
 from nonebot.adapters import Message
 from nonebot.params import CommandArg
 from nonebot.plugin import on_command
-from nonebot.adapters.onebot.v11 import Bot
-from nonebot.adapters.onebot.v11.event import MessageEvent
-from nonebot.adapters.onebot.v11.message import MessageSegment
+from nonebot.adapters.ntchat import Bot
+from GenshinUID.utils.db_operation.db_operation import select_db
+from nonebot.adapters.ntchat.message import Message, MessageSegment
+from nonebot.adapters.ntchat.event import MessageEvent, TextMessageEvent
 
 from .data_updater import updateCache
 from .data_source import getTeam, getPanel
-from .__utils__ import GSPANEL_ALIAS, uidHelper, formatTeam, formatInput, fetchInitRes
+from .__utils__ import GSPANEL_ALIAS, aliasWho, formatTeam, fetchInitRes
 
 driver = get_driver()
 driver.on_startup(fetchInitRes)
@@ -20,33 +24,69 @@ showTeam = on_command("teamdmg", aliases={"队伍伤害"}, priority=13, block=Tr
 
 uidStart = ["1", "2", "5", "6", "7", "8", "9"]
 
+# 尽量不改动除了__init__之外的文件
+async def formatInput(msg: str, wxid: str) -> Tuple[str, str]:
+    """
+    输入消息中的 UID 与角色名格式化，应具备处理 ``msg`` 为空、包含中文或数字的能力。
+    - 首个中文字符串捕获为角色名，若不包含则返回 ``all`` 请求角色面板列表数据
+    - 首个数字字符串捕获为 UID，若不包含则返回 ``uidHelper()`` 根据绑定配置查找的 UID
+
+    * ``param msg: str`` 输入消息，由 ``state["_prefix"]["command_arg"]`` 或 ``event.get_plaintext()`` 生成，可能包含 CQ 码
+    * ``param qq: str`` 输入消息触发 QQ
+    * ``param atqq: str = ""`` 输入消息中首个 at 的 QQ
+    - ``return: Tuple[str, str]``  UID、角色名
+    """  # noqa: E501
+    uid, char, tmp = "", "", ""
+    group = re.findall(
+        r"[0-9]+|[\u4e00-\u9fa5]+|[a-z]+", re.sub(r"\[CQ:.*\]", "", msg.split("@")[0]), flags=re.IGNORECASE
+    )
+    for s in group:
+        if s.isdigit():
+            if len(s) == 9:
+                if not uid:
+                    uid = s
+            else:
+                # 0人，1斗，97忍
+                tmp = s
+        elif s.encode().isalpha():
+            # dio娜，abd
+            tmp = s.lower()
+        elif not s.isdigit() and not char:
+            char = tmp + s
+    uid = uid or await select_db(wxid, mode='uid')
+    char = await aliasWho(char or tmp or "全部")
+    return uid, char
+
 
 @showPanel.handle()
-async def giveMePower(bot: Bot, event: MessageEvent, arg: Message = CommandArg()):
-    qq = str(event.get_user_id())
-    argsMsg = " ".join(seg.data["text"] for seg in arg["text"])
-    # 提取消息中的 at 作为操作目标 QQ
-    opqq = event.message["at"][0].data["qq"] if event.message.get("at") else ""
-    # 输入以「绑定」开头，识别为绑定操作
-    if argsMsg.startswith("绑定"):
-        args = [a for a in argsMsg[2:].split() if a.isdigit()]
-        if len(args) == 1:
-            uid, opqq = args[0], opqq or qq
-        elif len(args) == 2:
-            uid, opqq = args[0], (opqq or args[1])
-        else:
-            await showPanel.finish("绑定参数格式错误！", at_sender=True)
-        if opqq != qq and qq not in bot.config.superusers:
-            await showPanel.finish(f"没有权限操作 QQ{qq} 的绑定状态！", at_sender=True)
-        elif uid[0] not in uidStart or len(uid) != 9:
-            await showPanel.finish(f"UID 是「{uid}」吗？好像不对劲呢..", at_sender=True)
-        await showPanel.finish(await uidHelper(opqq, uid))
-    # 尝试从输入中理解 UID、角色名
-    uid, char = await formatInput(argsMsg, qq, opqq)
-    if not uid:
-        await showTeam.finish("要查询角色面板的 UID 捏？", at_sender=True)
+async def giveMePower(event: TextMessageEvent, args: Message = CommandArg()):
+    wxid_list = []
+    wxid_list.append(event.from_wxid)
+
+    raw_mes = args.extract_plain_text().strip()
+    name = ''.join(re.findall('[\u4e00-\u9fa5]', raw_mes))
+    if not name:
+        return
+    if "@" in raw_mes:
+        raw_mes = raw_mes.split("@")[0]
+        if not raw_mes:
+            return
+
+    wxid = event.from_wxid
+    if event.at_user_list:
+        for user in event.at_user_list:
+            user = user.strip()
+            if user != "":
+                wxid = user
+
+     # 尝试从输入中理解 UID、角色名
+    uid, char = await formatInput(raw_mes, wxid)
+
+    if '未找到绑定的UID' in uid:
+        await showTeam.finish(MessageSegment.room_at_msg(content=f"要查询角色面板的 UID 捏？"+"{$@}", at_list=wxid_list))
     elif not uid.isdigit() or uid[0] not in uidStart or len(uid) != 9:
-        await showPanel.finish(f"UID 是「{uid}」吗？好像不对劲呢..", at_sender=True)
+        await showPanel.finish(MessageSegment.room_at_msg(content=f"UID 是「{uid}」吗？好像不对劲呢.."+"{$@}", at_list=wxid_list))
+
     logger.info(f"正在查找 UID{uid} 的「{char}」角色面板..")
     rt = await getPanel(uid, char)
     if isinstance(rt, str):
@@ -56,19 +96,36 @@ async def giveMePower(bot: Bot, event: MessageEvent, arg: Message = CommandArg()
 
 
 @showTeam.handle()
-async def x_x(bot: Bot, event: MessageEvent, arg: Message = CommandArg()):
-    qq = str(event.get_user_id())
-    argsMsg = " ".join(seg.data["text"] for seg in arg["text"])
-    # 提取消息中的 at 作为操作目标 QQ
-    opqq = event.message["at"][0].data["qq"] if event.message.get("at") else ""
+async def x_x(bot: Bot, event: TextMessageEvent, args: Message = CommandArg()):
+    wxid_list = []
+    wxid_list.append(event.from_wxid)
+
+    raw_mes = args.extract_plain_text().strip()
+    name = ''.join(re.findall('[\u4e00-\u9fa5]', raw_mes))
+    if not name:
+        return
+    if "@" in raw_mes:
+        raw_mes = raw_mes.split("@")[0]
+        if not raw_mes:
+            return
+
+    wxid = event.from_wxid
+    if event.at_user_list:
+        for user in event.at_user_list:
+            user = user.strip()
+            if user != "":
+                wxid = user
+
     # 尝试从输入中理解 UID、角色名
-    uid, chars = await formatTeam(argsMsg, qq, opqq)
-    if not uid:
-        await showTeam.finish("要查询队伍伤害的 UID 捏？", at_sender=True)
+    uid, chars = await formatTeam(raw_mes, wxid)
+
+    if '未找到绑定的UID' in uid:
+        await showTeam.finish(MessageSegment.room_at_msg(content=f"要查询角色面板的 UID 捏？"+"{$@}", at_list=wxid_list))
     elif not uid.isdigit() or uid[0] not in uidStart or len(uid) != 9:
-        await showPanel.finish(f"UID 是「{uid}」吗？好像不对劲呢..", at_sender=True)
+        await showPanel.finish(MessageSegment.room_at_msg(content=f"UID 是「{uid}」吗？好像不对劲呢.."+"{$@}", at_list=wxid_list))
+
     if not chars:
-        logger.info(f"QQ{qq} 的输入「{argsMsg}」似乎未指定队伍角色！")
+        logger.info(f"微信 {wxid} ,UID:{uid} 的输入「{raw_mes}」似乎未指定队伍角色！")
     logger.info(f"正在查找 UID{uid} 的「{'/'.join(chars) or '展柜前 4 角色'}」队伍伤害面板..")
     rt = await getTeam(uid, chars)
     if isinstance(rt, str):
